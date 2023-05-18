69a36de487ea8864ba60a0b77c7f48580458a5da
BlueOrang3 <matealtunashvili@gmail.com>
2023-05-18 21:20:46 +0400

    feat: navbar component and global import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import type { AppProps } from 'next/app'
+import Navbar from "@/components/Navbar"
+import { GetServerSideProps } from "next"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { Toaster } from 'react-hot-toast';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Toaster />
+    </>
+  );
 }
