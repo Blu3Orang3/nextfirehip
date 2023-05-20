aa7210657a3380c055f5b65a415b7a881fee4d74
BlueOrang3 <matealtunashvili@gmail.com>
2023-05-21 01:11:15 +0400

    feat: get user and convert doc to json helpers

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -39,3 +39,32 @@ export const firestore = getFirestore(firebaseApp);
 // Storage exports
 export const storage = getStorage(firebaseApp);
 export const STATE_CHANGED = 'state_changed';
+
+/**`
+ * Gets a users/{uid} document with username
+ * @param  {string} username
+ */
+export async function getUserWithUsername(username) {
+
+  const q = query(
+    collection(firestore, 'users'), 
+    where('username', '==', username),
+    limit(1)
+  )
+  const userDoc = ( await getDocs(q) ).docs[0];
+  return userDoc;
+}
+
+/**`
+ * Converts a firestore document to JSON
+ * @param  {DocumentSnapshot} doc
+ */
+export function postToJSON(doc) {
+  const data = doc.data();
+  return {
+    ...data,
+    // Gotcha! firestore timestamp NOT serializable to JSON. Must convert to milliseconds
+    createdAt: data?.createdAt.toMillis() || 0,
+    updatedAt: data?.updatedAt.toMillis() || 0,
+  };
+}
