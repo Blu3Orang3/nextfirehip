ff0ea97ea2a95866e6460d527df8e0f68f7f563e
BlueOrang3 <matealtunashvili@gmail.com>
2023-05-21 18:14:45 +0400

    feat: bump firebase to v9 and 404 circuit

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,11 +1,21 @@
-import { getUserWithUsername, postToJSON } from '../../lib/firebase';
-import UserProfile from '../../components/UserProfile';
-import PostFeed from '../../components/PostFeed';
+import { getUserWithUsername, postToJSON, firestore } from '@/lib/firebase';
+import { query, collection, where, getDocs, limit, orderBy, getFirestore } from 'firebase/firestore';
+import UserProfile from '@/components/UserProfile';
+// import Metatags from '@/components/Metatags';
+import PostFeed from '@/components/PostFeed';
 
-export async function getServerSideProps({ query }) {
-  const { username } = query;
+
+export async function getServerSideProps({ query: urlQuery }) {
+  const { username } = urlQuery;
 
   const userDoc = await getUserWithUsername(username);
+
+  // If no user, short circuit to 404 page
+  if (!userDoc) {
+    return {
+      notFound: true,
+    };
+  }
 
   // JSON serializable data
   let user = null;
@@ -13,12 +23,13 @@ export async function getServerSideProps({ query }) {
 
   if (userDoc) {
     user = userDoc.data();
-    const postsQuery = userDoc.ref
-      .collection('posts')
-      .where('published', '==', true)
-      .orderBy('createdAt', 'desc')
-      .limit(5);
-    posts = (await postsQuery.get()).docs.map(postToJSON);
+    const postsQuery = query(
+      collection(getFirestore(), userDoc.ref.path, 'posts'),
+      where('published', '==', true),
+      orderBy('createdAt', 'desc'),
+      limit(5)
+    );
+    posts = (await getDocs(postsQuery)).docs.map(postToJSON);
   }
 
   return {
@@ -29,8 +40,9 @@ export async function getServerSideProps({ query }) {
 export default function UserProfilePage({ user, posts }) {
   return (
     <main>
+      {/* <Metatags title={user.username} description={`${user.username}'s public profile`} /> */}
       <UserProfile user={user} />
-      <PostFeed posts={posts} />
+      {/* <PostFeed posts={posts} /> */}
     </main>
   );
 }
