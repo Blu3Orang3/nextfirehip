d69219995271b3488143ff72eb4d18f3768e81a8
BlueOrang3 <matealtunashvili@gmail.com>
2023-05-22 01:49:22 +0400

    fix: legacy next link bump

--- components/PostFeed.tsx
+++ components/PostFeed.tsx
@@ -16,14 +16,12 @@ function PostItem({ post, admin = false }) {
   return (
     <div className='card'>
       <Link href={`/${post.username}`}>
-        <a>
-          <strong>By @{post.username}</strong>
-        </a>
+        <strong>By @{post.username}</strong>
       </Link>
 
       <Link href={`/${post.username}/${post.slug}`}>
         <h2>
-          <a>{post.title}</a>
+          {post.title}
         </h2>
       </Link>
 
@@ -53,19 +51,3 @@ function PostItem({ post, admin = false }) {
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
